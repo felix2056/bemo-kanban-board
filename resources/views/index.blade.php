c4ac3b621aa52766be1021718349490c7907be99
Daniel Felix <felixdaniel2056@gmail.com>
2023-01-16 21:14:13 +0100

    chore: changed title tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Add Bootstrap 4 to Laravel 8</title>
+    <title>Bemo Kanban App</title>
     <link href="/css/kanban.css" rel="stylesheet" type="text/css" />
 
     <!-- Fontawesome -->
