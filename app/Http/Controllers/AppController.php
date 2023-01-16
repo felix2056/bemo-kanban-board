19b974f542e6381eac0b64680e3e357d1be47f76
Daniel Felix <felixdaniel2056@gmail.com>
2023-01-16 20:46:29 +0100

    chore: sql dump not working correctly on server 2

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -11,14 +11,14 @@ class AppController extends Controller
         $database_name = env('DB_DATABASE');
         $username = env('DB_USERNAME');
         $password = env('DB_PASSWORD');
+        $path = storage_path();
 
         \Spatie\DbDumper\Databases\MySql::create()
             ->setDbName($database_name)
             ->setUserName($username)
             ->setPassword($password)
             ->includeTables(['columns', 'cards'])
-            // dump inside public folder
-            ->dumpToFile('public/' . $database_name . '.sql');
+            ->dumpToFile($path . '/' . $database_name . '.sql');
 
         return response()->download($database_name . '.sql');
     }
